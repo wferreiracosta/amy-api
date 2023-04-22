227c5aa9bc5bcabf3ca745e9e7a4a7ba8ca32ae2
Wesley Costa <wesferrcosta@gmail.com>
2023-04-21 22:51:22 -0300

    Criado repository e classe de model de categoria

---
+++ src/main/java/br/com/wferreiracosta/amy/models/Categoria.java
@@ -0,0 +1,32 @@
+package br.com.wferreiracosta.amy.models;
+
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@ToString
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    @NotNull
+    private String nome;
+
+}
